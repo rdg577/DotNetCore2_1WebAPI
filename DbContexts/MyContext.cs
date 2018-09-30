3a27032b37b324d58724d8a67e17f93b4854d746
Reden Gallera <rdg577@gmail.com>
2018-09-30 19:00:13 +0800

    Base stuffs for .Net Core 2.1 Web API...

---
+++ DbContexts/MyContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitedChange.DbContexts
+{
+    public class MyContext: DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options): base(options) { }
+    }
+}
